f0800dcbfa28f970dce3ffdb7894900ac92cba80
Rachel Opuba <ogbenirach@gmail.com>
2023-06-03 06:37:15 +0100

    Styled active nav link indicator and made some css cleanup

--- src/components/layouts/DashBoardContainer.js
+++ src/components/layouts/DashBoardContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, Outlet } from "react-router-dom";
 import { RiDashboardFill, RiAdminLine, RiHistoryFill } from "react-icons/ri";
 import { TfiPieChart } from "react-icons/tfi";
@@ -7,35 +7,73 @@ import { FiSettings, FiHelpCircle } from "react-icons/fi";
 import DbNav from "./DbNav";
 
 const DashBoardContainer = () => {
+  const [activeLink, setActiveLink] = useState(1);
+  const handleLinkClick = (pageId) => {
+    setActiveLink(pageId);
+  };
   return (
     <>
       <aside className="db-s-nav">
         <DbNav />
         <nav className="db-links">
-          <NavLink to="/dashboard" className="db-link">
+          <NavLink
+            to="/dashboard"
+            onClick={() => handleLinkClick(1)}
+            className={activeLink === 1 ? "active-dbl" : " db-link"}
+          >
             <RiDashboardFill /> Dashboard
           </NavLink>
-          <NavLink to="borrow" className="db-link">
+          <NavLink
+            to="borrow"
+            onClick={() => handleLinkClick(2)}
+            className={activeLink === 2 ? "active-dbl" : " db-link"}
+          >
             <TfiPieChart /> Borrow Data
           </NavLink>
-          <NavLink to="loan" className="db-link">
+          <NavLink
+            to="loan"
+            onClick={() => handleLinkClick(3)}
+            className={activeLink === 3 ? "active-dbl" : " db-link"}
+          >
             <TbCoins /> Loan Applications
           </NavLink>
-          <NavLink to="history" className="db-link">
+          <NavLink
+            to="history"
+            onClick={() => handleLinkClick(4)}
+            className={activeLink === 4 ? "active-dbl" : " db-link"}
+          >
             <RiHistoryFill /> History
           </NavLink>
-          <NavLink to="admin" className="db-link">
+          <NavLink
+            to="admin"
+            onClick={() => handleLinkClick(5)}
+            className={activeLink === 5 ? "active-dbl" : " db-link"}
+          >
             <RiAdminLine /> Admin
           </NavLink>
-          <NavLink to="settings" className="db-link">
+          <NavLink
+            to="settings"
+            onClick={() => handleLinkClick(6)}
+            className={activeLink === 6 ? "active-dbl" : " db-link"}
+          >
             <FiSettings /> Settings
           </NavLink>
-          <NavLink to="help" className="db-link"><FiHelpCircle/>Help & Support</NavLink>
-            </nav>
+          <NavLink
+            to="help"
+            onClick={() => handleLinkClick(7)}
+            className={activeLink === 7 ? "active-dbl" : " db-link"}
+          >
+            <FiHelpCircle />
+            Help & Support
+          </NavLink>
+        </nav>
 
-            <div>
-          <NavLink to="logout" className="db-link"><TbLogout/>LogOut</NavLink>
-          </div>
+        <div>
+          <NavLink to="/" className="db-link">
+            <TbLogout />
+            LogOut
+          </NavLink>
+        </div>
       </aside>
 
       <Outlet />
--- src/components/layouts/DbNav.js
+++ src/components/layouts/DbNav.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../../assets/Loan logo 1.png";
-import mail from "../../assets/mail-icon.svg";
-import bell from "../../assets/bell-icon.svg";
+// import mail from "../../assets/mail-icon.svg";
+// import bell from "../../assets/bell-icon.svg";
 import profile from "../../assets/profile-pic.png";
 import { NavLink } from "react-router-dom";
 
@@ -14,14 +14,16 @@ const DbNav = () => {
     <>
       <div className="db-nav-container">
         <div className="db-logo header-img">
-          <NavLink to="/"><img src={logo} /></NavLink>
+          <NavLink to="/">
+            <img src={logo} />
+          </NavLink>
         </div>
         <div className="db-search">
           <input type="text" placeholder="Search" className="db-input-search" />
         </div>
         <div className="db-nav-icons">
-          <img src={mail} className="db-mail" />
-          <img src={bell} className="db-bell" />
+          {/* <img src={mail} className="db-mail" />
+          <img src={bell} className="db-bell" /> */}
           <img src={profile} className="profile-pic" />
         </div>
       </div>
