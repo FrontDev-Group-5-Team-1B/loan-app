c0344d294cb87d8c712df80941816bcc2e3c2995
AnulikaP <anulinnedu@gmail.com>
2023-06-05 01:05:40 +0100

    preview

--- src/components/dashboard/BorrowerProfile.js
+++ src/components/dashboard/BorrowerProfile.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MdKeyboardArrowRight } from 'react-icons/md'
 import { RiArrowDownSLine } from 'react-icons/ri'
 import { Link } from 'react-router-dom'
 import "../../styles/borrowersProfile.css"
+import Modal from 'react-modal';
 
+
+
+Modal.setAppElement('#root');
 const BorrowerProfile = () => {
+    const [ModalIsopen, setModalisopen] = useState(false)
+
+    const customStyles = {
+        content: {
+          top: '50%',
+          left: '50%',
+          right: 'auto',
+          bottom: 'auto',
+          marginRight: '-50%',
+          transform: 'translate(-50%, -50%)',
+          padding: '50px'
+        },
+      };
 
 
   return (
@@ -19,9 +36,6 @@ const BorrowerProfile = () => {
                 <div>
                 <h4>Blessing Effiong</h4>
                 <p>Date: 15/02/23</p>
-                </div>
-                <div className='status-change'>
-                <p>Change Status<span><RiArrowDownSLine/></span></p>
                 </div>
                 
             </div>
@@ -39,8 +53,73 @@ const BorrowerProfile = () => {
                     <p>Admin in Charge:</p>
                     <p>Mabel Okeke</p>
                 </div>
-                <div className='borrow_link'>
-                    <Link to='/dashboard/borrow' className='borrow_link'><p>View Borrower's Data</p></Link>
+                <div>
+                <button onClick={() => setModalisopen(true)} className='borrow_preview'>View Borrower's Data</button>
+                
+                <Modal isOpen={ModalIsopen} onRequestClose={() => setModalisopen(false)}  style={customStyles} >
+                <div className='modalP-head'>
+                   <h2>Borrower's Saved Data</h2>
+                   <p>Personal and contact information</p>
+                   </div>
+                   <div className='modal-flex'>
+                    
+                        <div>
+                        <li>Miss Tumise Anikulapo</li>
+                        <li>anut@gmail.com</li>
+                        <li>45 Crescent Ikejs</li>
+                        <li>Unemployed</li>
+                        
+                        </div>
+                    
+                        <div>
+                        <li>+2348067532955</li>
+                        <li>Date of Birth</li>
+                        <li>13th August 1986</li>
+                        <li>.......</li>
+                        <li>10,000 Naira</li>
+                        
+                        </div>
+                   
+                   </div>
+                   <div className='modal-btn-div'>
+                  <Link to='/dashboard/borrow'> <button onClick={() => setModalisopen(false)} className='modal-btn'>Next</button></Link>
+                   </div>
+
+                </Modal>
+
+                </div>
+                <div>
+                
+                <Modal isOpen={ModalIsopen} onRequestClose={() => setModalisopen(false)}  style={customStyles} >
+
+                    <div className='modalP-head'>
+                   <h2>Borrower's Saved Data</h2>
+                   <p>Guarantor's Information</p>
+                   </div>
+                   <div className='modal-flex'>
+                    
+                        <div>
+                        <li>Name</li>
+                        <li>Email</li>
+                        <li>Address</li>
+                        <li>Relationship</li>
+                        <li>Other sources of income</li>
+                        </div>
+                    
+                        <div>
+                        <li>Phone Number</li>
+                        <li>Date of Birth</li>
+                        <li>Social Security Number</li>
+                        <li>Employment</li>
+                        <li>Income per month</li>
+                        
+                        </div>
+                   
+                   </div>
+                   <div className='modal-btn-div'>
+                  <Link to='/dashboard/borrow'> <button onClick={() => setModalisopen(false)} className='modal-btn'>Next</button></Link>
+                   </div>
+                   </Modal>
                 </div>
             </div>
 
