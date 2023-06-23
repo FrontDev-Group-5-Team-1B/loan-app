f4e098daf4f30d0e3319bd4ee9d0463fe70f45df
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-06-23 11:52:25 +0100

    successful loans

--- src/components/dashboard/LoansDeclined.js
+++ src/components/dashboard/LoansDeclined.js
@@ -78,27 +78,21 @@ const LoansDeclined = () => {
               </tr>
             </thead>
             <tbody>
-              {/* {DeclinedLoanData?.map((declinedData, index) => (
-                <tr key={index}>
-                  <td>{declinedData.borrower_name}</td>
-                  <td>{declinedData.date}</td>
-                  <td className="generated-red">{declinedData.status}</td>
-                  <td>{declinedData.credit_score}</td>
-                  <td>{declinedData.amount}</td>
-                </tr>
-              ))} */}
              
-              {declined?.data.loans.map((all) => {
-                return (
-                  <tr key={all._id}>
-                    <td>{all.fullname}</td>
-                    <td>{all.createdAt}</td>
-                    <td className="generated-red">Declined</td>
-                    <td>{all.creditScore}</td>
-                    <td>{all.loanAmount}</td>
-                  </tr>
-                );
-              })}
+             {declined?.data?.loans.length === 0 ? declined.message :
+             declined?.data?.loans.map((all) => {
+              return (
+                <tr key={all._id}>
+                  <td>{all.fullname}</td>
+                  <td>{all.createdAt}</td>
+                  <td className="generated-red">Declined</td>
+                  <td>{all.creditScore}</td>
+                  <td>{all.loanAmount}</td>
+                </tr>
+              );
+            })
+             }
+              
             </tbody>
           </table>
         </div>
